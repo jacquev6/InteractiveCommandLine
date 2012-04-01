eab33fce8a12097a6ff986c91ecc34cd59db877b
Vincent Jacques <vincent@vincent-jacques.net>
2012-04-01 20:15:26 +0100

    CommandContainer

--- InteractiveCommandLine.py
+++ InteractiveCommandLine.py
@@ -7,16 +7,17 @@ class Command:
     def addOption( self, name, handler ):
         pass
 
-class Program:
+class CommandContainer:
     def __init__( self ):
         self.__commands = dict()
 
-    def execute( self ):
-        self.executeWithArguments( *sys.argv[ 1: ] )
-
-    def executeWithArguments( self, *arguments ):
+    def execute( self, *arguments ):
         command = self.__commands[ arguments[ 0 ] ]
         command.execute( *arguments[ 1: ] )
 
     def addCommand( self, name, command ):
         self.__commands[ name ] = command
+        
+class Program( CommandContainer ):
+    def execute( self ):
+        CommandContainer.execute( self, *sys.argv[ 1: ] )
