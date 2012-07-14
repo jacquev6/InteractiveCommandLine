23d0ff2a84bd543ebec6c04a5dac6fdd59f81ca7
Vincent Jacques <vincent@vincent-jacques.net>
2012-07-14 09:38:42 +0200

    Implement command options

--- InteractiveCommandLine.UnitTest.py
+++ InteractiveCommandLine.UnitTest.py
@@ -6,31 +6,38 @@ from InteractiveCommandLine import *
 
 class CommandLineCommandExecution( unittest.TestCase ):
     def setUp( self ):
-        self.command = Command()
         self.optionHandler = MockMockMock.Mock( "optionHandler" )
-        self.command.addOption( "option", self.optionHandler )
-        self.execute = MockMockMock.Mock( "execute" )
-        self.command.execute = self.execute.object
+        self.commandHandler = MockMockMock.Mock( "commandHandler", self.optionHandler )
+
+        self.option = Option()
+        self.option.handle = self.optionHandler.object
+
+        self.command = Command()
+        self.command.addOption( "option", self.option )
+        self.command.handle = self.commandHandler.object
+
         self.program = Program()
         self.program.addCommand( "test", self.command )
 
     def tearDown( self ):
-        self.execute.tearDown()
-
-    def __executeProgram( self, *arguments ):
-        CommandContainer.execute( self.program, *arguments )
+        self.commandHandler.tearDown()
     
     def testWithoutArguments( self ):
-        self.execute.expect()
-        self.__executeProgram( "test" )
+        self.commandHandler.expect()
+        self.program._execute( "test" )
 
     def testWithArguments( self ):
-        self.execute.expect( "foo", "bar" )
-        self.__executeProgram( "test", "foo", "bar" )
+        self.commandHandler.expect( "foo", "bar" )
+        self.program._execute( "test", "foo", "bar" )
 
-    # def testWithOption( self ):
-        # self.optionHandler.expect()
-        # self.execute.expect()
-        # self.program.executeWithArguments( "test", "--option" )
+    def testWithOption( self ):
+        self.optionHandler.expect( [] ).andReturn( [] )
+        self.commandHandler.expect()
+        self.program._execute( "test", "--option" )
+
+    def testWithOptionWithArgument( self ):
+        self.optionHandler.expect( [ "value" ] ).andReturn( [] )
+        self.commandHandler.expect()
+        self.program._execute( "test", "--option", "value" )
 
 unittest.main()
--- InteractiveCommandLine.py
+++ InteractiveCommandLine.py
@@ -1,23 +1,47 @@
 import sys
 
-class Command:
-    def __init__( self ):
-        pass
+class Option:
+    def consumeArguments( self, arguments ):
+        return self.handle( arguments )
 
-    def addOption( self, name, handler ):
-        pass
+class OptionContainer:
+    def __init__( self ):
+        self.__options = dict()
+
+    def addOption( self, name, option ):
+        self.__options[ name ] = option
+
+    def consumeOptions( self, arguments ):
+        goOn = True
+        while goOn and len( arguments ) > 0:
+            goOn = False
+            argument = arguments[ 0 ]
+            if argument.startswith( "--" ):
+                optionName = argument[ 2: ]
+                if optionName in self.__options:
+                    arguments = self.__options[ optionName ].consumeArguments( arguments[ 1: ] )
+                    goOn = True
+        return arguments
+
+class Command( OptionContainer ):
+    def execute( self, arguments ):
+        arguments = self.consumeOptions( arguments )
+        self.handle( *arguments )
 
 class CommandContainer:
     def __init__( self ):
         self.__commands = dict()
 
-    def execute( self, *arguments ):
+    def execute( self, arguments ):
         command = self.__commands[ arguments[ 0 ] ]
-        command.execute( *arguments[ 1: ] )
+        command.execute( arguments[ 1: ] )
 
     def addCommand( self, name, command ):
         self.__commands[ name ] = command
-        
-class Program( CommandContainer ):
-    def execute( self ):
-        CommandContainer.execute( self, *sys.argv[ 1: ] )
+
+class Program( CommandContainer, OptionContainer ):
+    def _execute( self, *arguments ):
+        CommandContainer.execute( self, list( arguments ) )
+
+    def execute( self ): # pragma no cover
+        self._execute( sys.argv[ 1: ] )
